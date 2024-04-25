89e9601bd668d762229fbade1e9e46537382faf6
Teis Loyche <teisloyche@hotmail.com>
2024-04-25 10:28:02 +0200

    Put semantic Ui in main

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import "semantic-ui-css/semantic.min.css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
