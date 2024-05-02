1dfb08ef704943d0525f88bea3ed6ddc5100ec1c
Oskar Gester <gester.oskar@gmail.com>
2024-05-02 14:47:45 +0200

    added genres and icons to dropdown menu

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,15 +1,37 @@
-import { Dropdown } from "semantic-ui-react";
+import {
+  DropdownMenu,
+  DropdownItem,
+  DropdownHeader,
+  DropdownDivider,
+  Dropdown,
+} from "semantic-ui-react";
 
-const options = [
-  { key: 1, text: "adventure", value: 1 },
-  { key: 2, text: "family", value: 2 },
-  { key: 3, text: "startegy", value: 3 },
-];
 const Filter = () => {
   return (
     <div className="filter-container">
-      <div className="button-container">
-        <Dropdown clearable options={options} selection />
+      <div className="dropdown-filterd">
+        <Dropdown
+          text="Find games by genre"
+          icon="chess"
+          floating
+          labeled
+          button
+          className="icon"
+        >
+          <DropdownMenu>
+            <DropdownHeader icon="chess" content="  Search by Genre" />
+            <DropdownDivider />
+            <DropdownItem icon="quidditch" text="Family" />
+            <DropdownItem icon="handshake outline" text="Strategy" />
+            <DropdownItem icon="magic" text="Fantasy" />
+            <DropdownItem icon="user secret" text="Adventure" />
+            <DropdownItem icon="clone outline" text="Card Game" />
+            <DropdownItem icon="chess rook" text="Role Play" />
+            <DropdownItem icon="transgender alternate" text="Adult" />
+            <DropdownItem icon="smile outline" text="Kids" />
+            <DropdownItem icon="bug" text="Science fiction" />
+          </DropdownMenu>
+        </Dropdown>
       </div>
     </div>
   );
