a78e23a00c21a7922c5ea94f36b0d7a9edd06cd6
Mia Stefansdotter <stefansdotter@gmail.com>
2024-05-02 14:42:54 +0200

    first commit

---
+++ src/components/RolltheDiceButton.jsx
@@ -0,0 +1,9 @@
+import "/.RolltheDiceButton.css";
+
+const RolltheDice = () => {
+  return;
+  <img src="/images.RolltheDice" alt="rollthedice-img"></img>;
+  <button className="rollthedicebutton">Roll the dice!</button>;
+};
+
+export default RolltheDice;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,6 @@
 const Register = () => {
   return <div>Register</div>;
+  <RolltheDice />;
 };
 
 export default Register;
