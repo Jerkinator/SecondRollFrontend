834b93eac7669812849b033e76e9ce46f2f6316b
Mia Stefansdotter <stefansdotter@gmail.com>
2024-05-02 11:58:28 +0200

    working on reusable button

---
+++ src/components/ReusableButton.jsx
@@ -0,0 +1,7 @@
+import "./ReusableButton.css";
+
+const ReusableButton = ({ children }) => {
+  return <div className="reusable-button"></div>;
+};
+
+export default ReusableButton;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,11 @@
+import ReusableButton from "../components/ReusableButton";
+
 const Login = () => {
-  return <div>Login</div>;
+  return;
+  <>
+    <div>Login</div>
+    <ReusableButton>Log in</ReusableButton>
+  </>;
 };
 
 export default Login;
