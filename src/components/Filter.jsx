c20754d94416841fc4b0cb6ee1107b9939f7412d
Oskar Gester <gester.oskar@gmail.com>
2024-05-02 13:02:25 +0200

    started testing dropdown menu

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,14 +1,20 @@
+import { Dropdown } from "semantic-ui-react";
+
+const options = [
+  { key: 1, text: "adventure", value: 1 },
+  { key: 2, text: "family", value: 2 },
+  { key: 3, text: "startegy", value: 3 },
+];
 const Filter = () => {
   return (
     <div className="filter-container">
       <div className="button-container">
-        <button>Family</button>
-        <button>Action</button>
-        <button>Adventure</button>
-        <button>Strategy</button>
+        <Dropdown clearable options={options} selection />
       </div>
     </div>
   );
 };
 
 export default Filter;
+// must be a page showing every category when clicked
+// look up how to display categorys dynamic?
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import Filter from "../components/Filter";
+
 const Home = () => {
-  return <div className="home-container"></div>;
+  return (
+    <div className="home-container">
+      <Filter />
+    </div>
+  );
 };
 
 export default Home;
