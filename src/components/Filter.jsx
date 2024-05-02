6981ceff2fb143bfdedfec8a7fcbcb2f0a81b276
Oskar Gester <gester.oskar@gmail.com>
2024-05-02 11:31:59 +0200

    started building filter component

---
+++ src/components/Filter.jsx
@@ -0,0 +1,14 @@
+const Filter = () => {
+  return (
+    <div className="filter-container">
+      <div className="button-container">
+        <button>Family</button>
+        <button>Action</button>
+        <button>Adventure</button>
+        <button>Strategy</button>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
