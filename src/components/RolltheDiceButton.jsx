b81207db53121737eef87267a666d3dbb2e96e62
Mia Stefansdotter <stefansdotter@gmail.com>
2024-05-06 14:20:14 +0200

    forgot to save changes

--- src/components/RolltheDiceButton.jsx
+++ src/components/RolltheDiceButton.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useState } from "react";
 import "./RolltheDiceButton.css";
+
+function RolltheDice() {
+  const [rollDice, setRollDice] = useState(false);
+
+  function handleClick() {
+    setRollDice(true);
+  }
+}
 
 const RolltheDice = () => {
   return (
     <div className="dice-button">
-      <button>
+      <button onClick={() => handleClick(true)}>
         <img src="images\RolltheDice.png" alt="RTD-button"></img>
       </button>
     </div>
