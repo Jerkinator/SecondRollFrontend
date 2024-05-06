bb0711eed5db1b4a06e08cc33ecedf4b7f6b943b
Vurpas <erikengstrom164@gmail.com>
2024-05-06 14:04:30 +0200

    register is now possible and working with the database!

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,171 @@
-import RegisterForm from "../components/RegisterForm";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
+
+import ReusableButton from "../components/ReusableButton";
 
 const Register = () => {
-  return <div></div>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [adress_street, setAdress_street] = useState("");
+  const [adress_zip, setAdress_zip] = useState("");
+  const [adress_city, setAdress_city] = useState("");
+
+  const navigate = useNavigate();
+
+  const {
+    state: { user },
+    dispatch,
+  } = useContext(AuthContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (
+      !username ||
+      !password ||
+      !email ||
+      !firstName ||
+      !lastName ||
+      !phoneNumber ||
+      !adress_street ||
+      !adress_zip ||
+      !adress_city
+    ) {
+      alert("Please fill in all the fields");
+      return;
+    }
+
+    try {
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_API_URL}/auth/signup`,
+        {
+          username,
+          password,
+          email,
+          firstName,
+          lastName,
+          phoneNumber,
+          adress_street,
+          adress_zip,
+          adress_city,
+        }
+      );
+
+      dispatch({
+        type: "REGISTER",
+        payload: data,
+      });
+
+      // Redirects user to Home when logged in.
+      return navigate("/");
+    } catch (err) {
+      console.log("Error " + err);
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <form className="register-form" onSubmit={handleSubmit}>
+        <h1>Skapa ny användare</h1>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Lösenord:</label>
+          <input
+            type="password"
+            placeholder="Lösenord"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Email:</label>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Förnamn:</label>
+          <input
+            type="text"
+            placeholder="Förnamn"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Efternamn:</label>
+          <input
+            type="text"
+            placeholder="Efternamn"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Telefon:</label>
+          <input
+            type="text"
+            placeholder="Telefon"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Gatuadress:</label>
+          <input
+            type="text"
+            placeholder="Gatuadress"
+            value={adress_street}
+            onChange={(e) => setAdress_street(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Postnummer:</label>
+          <input
+            type="text"
+            placeholder="Postnummer"
+            value={adress_zip}
+            onChange={(e) => setAdress_zip(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Stad:</label>
+          <input
+            type="text"
+            placeholder="Stad"
+            value={adress_city}
+            onChange={(e) => setAdress_city(e.target.value)}
+          />
+        </div>
+        <ReusableButton type="submit">Registrera</ReusableButton>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
