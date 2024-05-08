b1f665d1a29d1dc79a61a58efdfda79a51e70bac
Mia Stefansdotter <stefansdotter@gmail.com>
2024-05-08 17:49:47 +0200

    will be trying with axios

--- src/components/RolltheDiceButton.jsx
+++ src/components/RolltheDiceButton.jsx
@@ -1,21 +1,37 @@
-import React from "react";
 import { useState, useEffect } from "react";
 import "./RolltheDiceButton.css";
 
-const RolltheDice = () => {
+const initRollDice = {
+  rollthedice: null,
+  website: null,
+};
+
+const Fetch = () => {
   const [rollDice, setRollDice] = useState(false);
 
+  const getRollDice = async () => {
+    const response = await fetch(
+      $({ import: meta.env.VITE_API_URL } / gameAds / rolldice)
+    );
+    const json = await response.json();
+
+    setRollDice({
+      gameAds: json.secondRoll,
+      website: json.secondroll,
+    });
+  };
+
   useEffect(() => {
-    // the code we want to run
-    console.log("Showing game ad:", rollDice);
-
-    // optional return function
+    getRollDice();
   }, []); // the dependency array
+};
 
+const RolltheDice = () => {
   return (
     <div className="container">
-      <button>
+      <button onClick={() => setRollDice(true)}>
         <img src="images\RolltheDice.png" alt="rtd-button"></img>
+        <div>{`gameAds: ${RolltheDice}`}</div>
       </button>
     </div>
   );
