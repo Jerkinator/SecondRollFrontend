939224a6c9ec478477763de26797673ba91d3b64
Mia Stefansdotter <stefansdotter@gmail.com>
2024-05-09 17:08:51 +0200

    having problems with axios, will be trying fetch instead

--- src/components/RolltheDiceButton.jsx
+++ src/components/RolltheDiceButton.jsx
@@ -1,40 +1,40 @@
 import { useState, useEffect } from "react";
 import "./RolltheDiceButton.css";
+import axios from "axios";
 
 const initRollDice = {
   rollthedice: null,
   website: null,
 };
 
-const Fetch = () => {
-  const [rollDice, setRollDice] = useState(false);
+const getRollDice = async () => {
+  const response = await fetch(
+    $({ import: meta.env.VITE_API_URL } / gameAds / rolldice)
+  );
+  const json = await response.json();
 
+  setRollDice({
+    gameAds: json.secondRoll,
+    website: json.secondroll,
+  });
+};
+
+const RolltheDice = () => {
+  const [rollDice, setRollDice] = useState([false]);
   const getRollDice = async () => {
-    const response = await fetch(
-      $({ import: meta.env.VITE_API_URL } / gameAds / rolldice)
-    );
-    const json = await response.json();
-
-    setRollDice({
-      gameAds: json.secondRoll,
-      website: json.secondroll,
-    });
+    await axios.get(`https://localhost:8080/api/gameads`);
   };
 
   useEffect(() => {
     getRollDice();
-  }, []); // the dependency array
-};
+  }, []); // the dependency array, on click??
 
-const RolltheDice = () => {
   return (
     <div className="container">
-      <button onClick={() => setRollDice(true)}>
+      <button>
         <img src="images\RolltheDice.png" alt="rtd-button"></img>
-        <div>{`gameAds: ${RolltheDice}`}</div>
       </button>
     </div>
   );
 };
-
 export default RolltheDice;
