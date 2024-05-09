6133b290dbfba76c0ca0369b6a0fd0d49efd44a1
Mia Stefansdotter <stefansdotter@gmail.com>
2024-05-09 18:35:31 +0200

    getting auth error message in backedn  when trying to make a get request

--- src/components/RolltheDiceButton.jsx
+++ src/components/RolltheDiceButton.jsx
@@ -2,36 +2,23 @@ import { useState, useEffect } from "react";
 import "./RolltheDiceButton.css";
 import axios from "axios";
 
-const initRollDice = {
-  rollthedice: null,
-  website: null,
-};
-
-const getRollDice = async () => {
-  const response = await fetch(
-    $({ import: meta.env.VITE_API_URL } / gameAds / rolldice)
-  );
-  const json = await response.json();
-
-  setRollDice({
-    gameAds: json.secondRoll,
-    website: json.secondroll,
-  });
-};
-
 const RolltheDice = () => {
   const [rollDice, setRollDice] = useState([false]);
-  const getRollDice = async () => {
-    await axios.get(`https://localhost:8080/api/gameads`);
-  };
 
-  useEffect(() => {
-    getRollDice();
-  }, []); // the dependency array, on click??
+  const handleOnClick = (setRollDice = true) => {
+    useEffect(() => {
+      axios
+        .get(`http://localhost:8080/api/gameads/all`, { withCredentials: true })
+        .then((res) => {
+          setRollDice(res.data);
+        })
+        .catch((err) => console.log(err));
+    }, []);
+  };
 
   return (
     <div className="container">
-      <button>
+      <button onClick={setRollDice}>
         <img src="images\RolltheDice.png" alt="rtd-button"></img>
       </button>
     </div>
