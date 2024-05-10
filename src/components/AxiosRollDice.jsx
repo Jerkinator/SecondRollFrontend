0b2189e4bf55c0d3ff9a992d993177768fdf9538
Mia Stefansdotter <stefansdotter@gmail.com>
2024-05-10 19:33:29 +0200

    fixed ternary for onClick, inserted game info in the return statement so it shows relevant info for a game ad

--- src/components/AxiosRollDice.jsx
+++ src/components/AxiosRollDice.jsx
@@ -4,32 +4,44 @@ import "./AxiosRollDice.css";
 
 const AxiosRollDice = ({ randomGame }) => {
   const [rollDice, setRollDice] = useState(false);
+  const [result, setResult] = useState("");
 
   const getRandomGameAd = () => {
-    axios
-      .get(`http://localhost:8080/api/gameAds/rolldice`)
-      .then((res) => {
-        console.log(res.data);
-        const randomGame = res.data;
-        setRollDice(randomGame);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    if (rollDice) {
+      axios
+        .get(`http://localhost:8080/api/gameAds/rolldice`)
+        .then((res) => {
+          console.log(res.data);
+          setResult(res.data);
+          setRollDice(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   };
 
-  const toggleFunc = () => {
-    setRollDice(!rollDice);
-  };
-
-  useEffect(() => getRandomGameAd(), []);
+  useEffect(() => getRandomGameAd(), [rollDice]);
 
   return (
-    <div className={rollDice ? getRandomGameAd : rollDice}>
-      <button onClick={toggleFunc}>
-        <img src="images\RolltheDice.png" alt="rtd-button"></img>
-      </button>
-      <div></div>
+    <div>
+      <div className="container">
+        <button onClick={() => setRollDice(true)}>
+          <img src="images\RolltheDice.png" alt="rtd-button"></img>
+        </button>
+      </div>
+      <div className="gameInfo">
+        <div>Titel: {result.title}</div>
+        <div>Pris: {result.price}</div>
+        <div>Säljare: {result.seller}</div>
+        <div>Tillverkare: {result.gameCreator}</div>
+        <div>Speltid: {result.gamePlayTime}</div>
+        <div>Rekommenderad ålder: {result.gameRecommendedAge}</div>
+        <div>Antal spelare: {result.gamePlayers}</div>
+        <div>
+          <img src={result.photoURL}></img>
+        </div>
+      </div>
     </div>
   );
 };
