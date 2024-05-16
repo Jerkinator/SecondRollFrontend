2a690f85209b8d131003f24551cd29e589540faa
Oskar Gester <gester.oskar@gmail.com>
2024-05-16 13:54:22 +0200

    added user id in GameAdResponse in backend, and ing gameAd controller

--- src/components/ViewCart.jsx
+++ src/components/ViewCart.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import React from "react";
 
 const ViewCart = () => {
@@ -10,18 +11,28 @@ const ViewCart = () => {
     console.log(cartItems);
   };
 
+  /*   useEffect(() => {
+    if (gameAd === "") {
+      return <h1>korgen är tom</h1>;
+    } */
+  /*  if (gameAd !== "") {
+       showCart(); 
+    } 
+  }, []);*/
+
   return (
     <div>
       <div>
+        <button onClick={showCart}>tryck här</button>
         {gameAd?.map((g) => (
           <div className="filter-container" key={g.id}>
             <img src={g.photoURL} alt="" key={g.id} />
             <p>Titel: {g.title}</p>
-            <p>Info: {g.description}</p>
+            <p>Info: {g.price}</p>
+            <p>Säljare: {g.seller}</p>
           </div>
         ))}
       </div>
-      <button onClick={showCart}> visakorg </button>
     </div>
   );
 };
