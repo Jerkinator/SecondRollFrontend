f3993aecbc63ccda070e0eebed47dd6100d67b55
Vurpas <erikengstrom164@gmail.com>
2024-05-17 09:37:07 +0200

    created faq about and contact page with dumb text

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,10 @@ import Register from "./pages/Register";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import CreateGameAd from "./pages/CreateGameAd";
-import Contact from "./pages/Contact";
 import GameAds from "./pages/GameAds";
 import Shoppingcart from "./pages/Shoppingcart";
 import RateUser from "./pages/RateUser";
 import SellerProfile from "./pages/SellerProfile";
-import Faq from "./pages/Faq";
 import Wishlist from "./pages/Wishlist";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
@@ -42,13 +40,48 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <Profile />
+              </PrivateRoute>
+            }
+          />
           <Route path="/gameads" element={<GameAds />} />
           <Route path="/gameads/:id" element={<GameAdDetails />} />
-          <Route path="/shoppingcart" element={<PrivateRoute><Shoppingcart /></PrivateRoute>} />
-          <Route path="/rateuser" element={<PrivateRoute><RateUser /></PrivateRoute>} />
-          <Route path="/sellerprofile" element={<PrivateRoute><SellerProfile /></PrivateRoute>} />
-          <Route path="/wishlist" element={<PrivateRoute><Wishlist /></PrivateRoute>} />
+          <Route
+            path="/shoppingcart"
+            element={
+              <PrivateRoute>
+                <Shoppingcart />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/rateuser"
+            element={
+              <PrivateRoute>
+                <RateUser />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/sellerprofile"
+            element={
+              <PrivateRoute>
+                <SellerProfile />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/wishlist"
+            element={
+              <PrivateRoute>
+                <Wishlist />
+              </PrivateRoute>
+            }
+          />
           <Route
             path="/creategamead"
             element={
--- src/pages/Faq.jsx
+++
@@ -1,5 +0,0 @@
-function Faq() {
-  return <p>Hello</p>;
-}
-
-export default Faq;
