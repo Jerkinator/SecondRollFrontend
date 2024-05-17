e39d9ccd93e66fc0ac456a9bb0b947fcb3cbbce2
Oskar Gester <gester.oskar@gmail.com>
2024-05-17 11:51:33 +0200

    working on retrieving gameAd id and user id in ViewCart.jsx

--- src/components/ViewCart.jsx
+++ src/components/ViewCart.jsx
@@ -4,11 +4,22 @@ import React from "react";
 
 const ViewCart = () => {
   const [gameAd, setGameAd] = useState([]);
+  const [order, setOrder] = useState([]);
   const cartItems = JSON.parse(localStorage.getItem("cart"));
+  const buyerId = JSON.parse(localStorage.getItem("user"));
 
   const showCart = (e) => {
     setGameAd(cartItems);
     console.log(cartItems);
+    console.log(buyerId);
+  };
+ const adId = gameAd?.map((gameAd.id) => (
+setOrder(adId)
+ ));
+
+  const createOrder = (e) => {
+    setOrder(adId);
+    console.log(adId);
   };
 
   /*   useEffect(() => {
@@ -23,7 +34,7 @@ const ViewCart = () => {
   return (
     <div>
       <div>
-        <button onClick={showCart}>tryck här</button>
+        <button onClick={createOrder}>tryck här</button>
         {gameAd?.map((g) => (
           <div className="filter-container" key={g.id}>
             <img src={g.photoURL} alt="" key={g.id} />
