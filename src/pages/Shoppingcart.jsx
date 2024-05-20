2a3e2e09a57a2e296be5cac103a275b9c465bbe4
Oskar Gester <gester.oskar@gmail.com>
2024-05-20 12:41:45 +0200

    got create order working with some help. need to clear cart after order submit

--- src/pages/Shoppingcart.jsx
+++ src/pages/Shoppingcart.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import React from "react";
 
@@ -8,43 +9,64 @@ import React from "react";
   -Empty Cart message that becomes hidden when Cart is not empty.*/
 
 const Shoppingcart = () => {
-  const [gameAd, setGameAd] = useState([]);
-  const [order, setOrder] = useState([]);
+  // const [gameAd, setGameAd] = useState([]);
+  const [order, setOrder] = useState({
+    buyerId: "",
+    gameAdIds: [],
+  });
+
+  const navigate = useNavigate();
 
   const cartItems = JSON.parse(localStorage.getItem("cart"));
+  // i cartItems finns allt men du vill filtrera nerså du bara har id (filter() eller kanske find())
+  // gör ny variabel och spara ner det filtrerade värdet
+  /* const gameIds = cartItems.find(cart.id);*/
+  const gameIds = cartItems.map((a) => a.id);
+
   const buyer = JSON.parse(localStorage.getItem("user"));
+  // samma sak gäller för user du vill bara ha id
+  /* const buyerId = buyer.get(); */
+  /* const buyerId = buyer.map((a) => a.id);
+  console.log("Buyer Id: " + buyerId); */
+  /* 
+  console.log(
+    "gameid: " + gameIds,
+    "buyer id: " + buyer.id,
+    "inside cart: " + cartItems
+  ); */
 
-  const showCart = (e) => {
-    setGameAd(cartItems);
-    console.log(cartItems);
-    console.log(buyer);
-  };
-  /*  const adId = gameAd?.map((gameAd.id) => (
-    setOrder(adId)
- )); */
-
-  /*  const adId = gameAd.map((gameAd) => {
-    setOrder(gameAd.id);
-    console.log(adId);
-  });
-  const showId = (e) => {
-    console.log(adId);
-  };
+  /*  useEffect(() => {
+    setOrder({ buyerId: buyer.id, gameAdIds: gameIds });
+  }, []);
  */
-  /*   useEffect(() => {
-    if (gameAd === "") {
-      return <h1>korgen är tom</h1>;
-    } */
-  /*  if (gameAd !== "") {
-       showCart(); 
-    } 
-  }, []);*/
+  const createOrder = async () => {
+    // setOrder({ buyerId: buyer.id, gameAdIds: gameIds });
+    // console.log(order);
+    // setOrder({ buyerId: buyer.id, gameAdIds: gameIds });
+    try {
+      await axios.post(
+        `${import.meta.env.VITE_API_URL}/orders`,
+        {
+          buyerId: buyer.id,
+          gameAdIds: gameIds,
+        },
+        { withCredentials: true }
+      );
+
+      alert("Order created!");
+
+      // Redirects user to Home when logged in.
+      return navigate("/");
+    } catch (err) {
+      console.log("Error " + err);
+    }
+  };
 
   return (
     <div>
+      <button onClick={createOrder}>Order Items</button>
       <div>
-        <button onClick={showCart}>tryck här</button>
-        {gameAd?.map((g) => (
+        {cartItems?.map((g) => (
           <div className="filter-container" key={g.id}>
             <img src={g.photoURL} alt="" key={g.id} />
             <p>Titel: {g.title}</p>
@@ -57,3 +79,29 @@ const Shoppingcart = () => {
   );
 };
 export default Shoppingcart;
+
+/* const showCart = (e) => {
+    setGameAd(cartItems);
+    // console.log("cart item: " + cartItems);
+    // console.log("buyer: " + JSON.parse(buyer));
+  }; */
+/*  const adId = gameAd?.map((gameAd.id) => (
+    setOrder(adId)
+ )); */
+
+/*  const adId = gameAd.map((gameAd) => {
+    setOrder(gameAd.id);
+    console.log(adId);
+  });
+  const showId = (e) => {
+    console.log(adId);
+  };
+ */
+/*   useEffect(() => {
+    if (gameAd === "") {
+      return <h1>korgen är tom</h1>;
+    } */
+/*  if (gameAd !== "") {
+       showCart(); 
+    } 
+  }, []);*/
